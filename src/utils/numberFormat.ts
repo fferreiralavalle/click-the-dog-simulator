97da5256a0ad0872716c0588d7b2635406ee1d29
fferreirala <fferreiralavalle@gmail.com>
2020-02-28 00:00:22 -0300

    Made all formated meassures uppdercase

--- src/utils/numberFormat.ts
+++ src/utils/numberFormat.ts
@@ -1,5 +1,5 @@
 import numeral from 'numeral'
 
 export const toFormat = (number: number): string => {
-    return numeral(number).format('0.[00]a')
+    return numeral(number).format('0.[00]a').toUpperCase()
 }
